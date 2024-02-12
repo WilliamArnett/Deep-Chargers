f34fd283be21da3a06a3c1061a71ec3c9821f6e9
William Arnett <arnettwilliam7@gmail.com>
2024-02-12 17:57:35 -0600

    draw loop, entity and player class

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -1,28 +1,39 @@
 package main;
 
 import javax.swing.JPanel;
+
+import entity.Player;
+
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class GamePanel extends JPanel implements Runnable{
 
     //Set screen settings
-    final int originalTileSize = 16;
+    final int originalTileSize = 20;
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    public final int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 16;
     final int screenWidth = tileSize*maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this,keyH);
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         //this.setPreferredSize(new Dimension(x,y));
         this.setBackground(Color.gray);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){ 
@@ -33,6 +44,46 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+        while(gameThread != null){
+            
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+            
+            if(timer >= 100000000){
+                System.out.println("FPS:" + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
+        }
+    }
+    public void update(){
+        player.update();
+
+    }
+    public void paintComponent(Graphics g){
+        
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        player.draw(g2);
+
+        g2.dispose();
     }
 }
--- main/Main.java
+++ main/Main.java
@@ -16,5 +16,6 @@ public class Main {
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+        gamePanel.startGameThread();
     }
 }
